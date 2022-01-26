b8ae84f91a10615ccb790ccb4cd0c918051c19dc
Andriy Kostenko <a.kostenkouk@gmail.com>
2022-01-26 15:30:44 +0200

    first commit

---
+++ app/forms.py
@@ -0,0 +1,66 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
+from app.models import User
+
+
+class EmptyForm(FlaskForm):
+    title = TextAreaField('Write the title:', validators=[DataRequired(), Length(min=1, max=50)])
+    post = TextAreaField('Write the post:', validators=[DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField('Submit')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign In')
+
+    def validate_submit(self, submit):
+        user = User.query.filter_by(email=self.email.data).first()
+        if not user or not user.check_password(self.password.data):
+            raise ValidationError('Email and password do not match')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+
+
+class PostForm(FlaskForm):
+    post_title = TextAreaField('Write the title:', validators=[DataRequired(), Length(min=1, max=50)])
+    post = TextAreaField('Write the post:', validators=[DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField('Submit')
+
+
+class DeleteForm(FlaskForm):
+    submit = SubmitField('Delete')
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username.')
